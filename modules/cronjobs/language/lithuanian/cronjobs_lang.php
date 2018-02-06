4a4d721c8174e0435e9ab1a13fd1c6fa3ed83f4e
Donatas Glodenis <dgvirtual@akl.lt>
2018-02-06 11:50:56 +0000

    Initial translation of file to Lithuanian

---
+++ modules/cronjobs/language/lithuanian/cronjobs_lang.php
@@ -0,0 +1,14 @@
+<?php
+$lang['module_cronjobs'] = 'Cron užduotys';
+$lang['cronjobs_instructions'] = 'Žemiau rasite cron užduotis (crontab), kurias galima naudoti vykdant nurodytas užduotis periodiškai. Pavyzdžiui, cron užduotys tobulai tinka
+automatinių duomenų bazės atsarginių kopijų kūrimui. Be to, mes net turime pavyzdį Jums adresu <strong>'.INSTALL_ROOT.'crons'.DIRECTORY_SEPARATOR.'crontab_default.php</strong>.
+Rekomenduojame <a href="http://www.google.com/search?client=safari&rls=en-us&q=cron+job+tutorial&ie=UTF-8&oe=UTF-8" target="_blank"><strong>spausti čia, jei norite daugiau sužinoti apie cron užduotis</strong></a>.';
+$lang['btn_remove_cronjobs'] = 'Pašalinti cron užduotį(-is)';
+$lang['btn_save_cronjobs'] = 'Įrašyt cron užduotį(-s)';
+$lang['cronjobs_mailto'] = 'Siųsti:';
+$lang['cronjobs_min'] = 'min';
+$lang['cronjobs_hour'] = 'val';
+$lang['cronjobs_month_day'] = 'mėnuo diena';
+$lang['cronjobs_month_num'] = 'mėnesio numeris';
+$lang['cronjobs_week_day'] = 'savaitės diena';
+$lang['cronjobs_command'] = 'komanda';

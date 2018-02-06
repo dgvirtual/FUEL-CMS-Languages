6768f631836032d7844503e27dd6594f54a28ae3
Donatas Glodenis <dgvirtual@akl.lt>
2018-02-06 12:44:25 +0200

    initial translation of two files to Lithuanian

---
+++ modules/fuel/language/lithuanian/fuel_inline_edit_lang.php
@@ -0,0 +1,24 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Inline Edit Toolbar
+|--------------------------------------------------------------------------
+*/
+$lang['inline_edit_toggle_toolbar'] = 'Rodyti/slėpti įrankių juostą';
+$lang['inline_edit_toggle_editable'] = 'Rodyti/slėpti redaguojamas sritis';
+$lang['inline_edit_toggle_publish'] = 'Rodyti/slėpti puslapio publikavimo būklę';
+$lang['inline_edit_toggle_cache'] = 'Rodyti/slėpti puslapio cache statusą';
+
+$lang['inline_edit_tools'] = 'Įrankiai...';
+$lang['inline_edit_other_pages'] = 'Kiti puslapiai...';
+$lang['inline_edit_layout'] = 'Išdėstymas';
+
+$lang['inline_edit_back_to_admin'] = 'Grįžti į administravimo pultą';
+$lang['inline_edit_logout'] = 'Atsijungti';
+$lang['inline_edit_logout_title'] = 'Atsijungti nuo FUEL administravimo pulto';
+$lang['inline_edit_login'] = 'Prisijungti';
+$lang['inline_edit_login_title'] = 'Prisijungti prie FUEL administravimo';
+$lang['inline_edit_page_not_published'] = 'Puslapis nepublikuotas';
+
+/* End of file fuel_inline_edit_lang.php */
+/* Location: ./modules/fuel/language/lithuanian/fuel_inline_edit_lang.php */
---
+++ modules/fuel/language/lithuanian/fuel_js_lang.php
@@ -0,0 +1,104 @@
+<?php 
+/*
+|--------------------------------------------------------------------------
+| Misc. For Javascript Files
+|--------------------------------------------------------------------------
+*/
+$lang['label_search'] = 'Ieškoti...';
+
+$lang['btn_save'] = 'Įrašyti';
+$lang['btn_cancel'] = 'Atšaukti';
+
+$lang['btn_add'] = 'Pridėti';
+$lang['btn_edit'] = 'Keisti';
+
+$lang['btn_select'] = 'Pažymėti';
+$lang['btn_asset'] = 'Įkelti failai';
+$lang['btn_image'] = 'Paveikslėlis';
+$lang['btn_pdf'] = 'PDF';
+$lang['btn_flash'] = 'Flash';
+$lang['btn_upload_asset'] = 'Įkelti failą';
+$lang['btn_view_source'] = 'Šaltinis';
+$lang['btn_view_editor'] = 'Rengyklė';
+$lang['btn_preview'] = 'Peržiūra';
+$lang['btn_select_all'] = 'Pažymėti viską';
+$lang['btn_deselect_all'] = 'Panaikinti pažymėjimą';
+
+$lang['comboselect_values_empty'] = 'Nebėra verčių, kurias galėtumėte pasirinkti';
+$lang['comboselect_selected_empty'] = 'Pasirinkite iš verčių kairėje';
+$lang['comboselect_filter'] = 'Filtruoti paiešką';
+$lang['tree_orphans'] = '_našlaičiai';
+
+$lang['confirm_delete'] = 'Ar tikrai norite tai pašalinti?';
+$lang['confirm_asset_remove'] = 'Ar tikrai norite pašalinti šį failą?';
+
+$lang['form_label_send_email_notification'] = 'Siūsti slaptažodžio atnaujinimo pranešimą el. paštu?';
+
+$lang['error_importing_ajax'] = 'Nepavyko importuoti „view“ failo';
+
+$lang['viewpage_close'] = 'Užverti';
+$lang['viewpage_new_window'] = 'Naujas langas';
+
+$lang['rearrange_on'] = 'Galite pakeisti rodymo tvarką pasirinkdami ir tempdami žemiau esančias duomenų eilutes.';
+
+$lang['replace_warning'] = 'Ar tikrai norite pakeisti šio įrašo turinį?';
+
+$lang['edit_multi_select_warning'] = 'Norint keisti įrašą turi būti pažymėtas tik jis vienas.';
+
+$lang['filters_close'] = '&#8673; užverti';
+$lang['filters_open'] = '&#8675; atverti';
+
+
+/*
+|--------------------------------------------------------------------------
+| MarkItUp
+|--------------------------------------------------------------------------
+*/
+$lang['markitup_b'] = 'Pusjuodis';
+$lang['markitup_i'] = 'Kursyvas';
+$lang['markitup_strike'] = 'Išbraukimas';
+$lang['markitup_p'] = 'Pastraipa';
+$lang['markitup_h1'] = 'Antraštė 1';
+$lang['markitup_h2'] = 'Antraštė 2';
+$lang['markitup_h3'] = 'Antraštė 3';
+$lang['markitup_h4'] = 'Antraštė 4';
+$lang['markitup_ul'] = 'Nenumeruotas sąrašas';
+$lang['markitup_ol'] = 'Numeruotas sąrašas';
+$lang['markitup_li'] = 'Sąrašo eilutė';
+$lang['markitup_blockquote'] = 'Citata';
+$lang['markitup_hr'] = 'Horizontalus brūkšnys';
+$lang['markitup_img'] = 'Paveikslėlis';
+$lang['markitup_link'] = 'Nuoroda';
+$lang['markitup_target'] = 'Taikinys';
+$lang['markitup_title'] = 'Pavadinimas';
+$lang['markitup_mailto'] = 'Kam siųsti';
+$lang['markitup_php'] = 'PHP kintamasis';
+$lang['markitup_clean'] = 'Išvalyti';
+$lang['markitup_preview'] = 'Peržiūros perjungimas (norėdami peržiūrėti laikykite nuspaudę Alt(Lyg3)/Option mygtuką ir spragtelėkite)';
+$lang['markitup_fullscreen'] = 'Visame ekrane';
+
+// placeholders
+$lang['markitup_placeholder_headings'] = 'Čia rašykite pavadinimą...';
+$lang['markitup_placeholder_link'] = 'Jūsų tekstas nuorodai...';
+$lang['markitup_email'] = 'vardas@adresas.lt';
+$lang['markitup_fullscreen_close'] = 'Užverti';
+
+
+/*
+|--------------------------------------------------------------------------
+| Repeatable
+|--------------------------------------------------------------------------
+*/
+$lang['warn_before_delete_msg'] = 'Ar tikrai norite ištrinti šį įrašą?';
+$lang['btn_add_another'] = 'Pridėti kitą';
+$lang['btn_remove'] = 'Pašalinti';
+
+/*
+|--------------------------------------------------------------------------
+| Assets
+|--------------------------------------------------------------------------
+*/
+$lang['assets_need_to_save'] = 'Norėdami naudoti šį failą spragtelėkite „Įrašyti“.';
+
+/* End of file fuel_js_lang.php */
+/* Location: ./modules/fuel/language/lithuanian/fuel_js_lang.php */
